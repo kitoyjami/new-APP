8ddbf34694a256cd3193df1fec7e557fb89b4221
kitoyjami <weurbanor@unitru.edu.pe>
2024-06-05 01:28:05 -0500

    create a new app

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Open up App.js to start working on your </Text>
+      <Text style={styles.titleText2}>app eddy miñano pendejo!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color:'white'
+  },
+  titleText2: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color:'red'
   },
 });
+
+
+
